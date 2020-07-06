6a4ba45bb5c841bc414ad21fc32d32200ee6617d
Sylvia <subin3311@gmail.com>
2020-07-07 01:13:02 +1200

    Add vehicle icons and click event

--- dist/js/main.js
+++ dist/js/main.js
@@ -2,20 +2,56 @@ var vehiclesArray = [];
 
 // INITIALISE ---------------------------------------------------------------- // 
 
-function init() {
+function init () {
     $.getJSON('/dist/json/vehicles.json', function(data) {
         vehiclesArray = data.vehicles;
+        displayVehicles(vehiclesArray);
     });
-    initValidation();
 };
 
 
 
-// VALIDATE FORM ------------------------------------------------------------- //
+// DISPLAY VEHICLES ---------------------------------------------------------- //
 
-function initValidation() {
-    
+function displayVehicles (vehicles) {
+    let el_vehicleList = $('.landing__icons');
+    var html = '';
+    $.each(vehicles, function (i, vehicle) {
+        html += makeListHTML(vehicle);
+    });
+    el_vehicleList.html(html);
+    addIconClickListener();
 }
+
+function makeListHTML (vehicle) {
+    return `
+    <div class="icon" data-id="${vehicle.id}">
+        <img src="/dist/image/icon${vehicle.id}.png" alt="${vehicle.title}">
+        <p></p>
+    </div>
+    `
+}
+
+function addIconClickListener () {
+    $('.icon').on('click hover', function() {
+        var id = $(this).data('id');
+        viewVehicleInfo(id);
+    });
+}
+
+
+// VIEW VEHICLES ------------------------------------------------------------- //
+
+
+function viewVehicleInfo (id) {
+    $.each(vehiclesArray, function(i, vehicle) {
+        if (vehicle.id == id) {
+            let vehicle = vehiclesArray[i];
+            $('.landing__info').html(vehicle.mintraveler + " ~ " + vehicle.maxtraveler + " people");
+        }
+    })
+}
+
 
 
 // RUN ----------------------------------------------------------------------- // 
