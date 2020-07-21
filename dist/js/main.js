e2e37f88fdae19394feff403a720f5615bca4b94
Sylvia <subin3311@gmail.com>
2020-07-22 00:18:19 +1200

    Get location values

--- dist/js/main.js
+++ dist/js/main.js
@@ -11,10 +11,12 @@ $(function init () {
     });
     initFilter();
     initScreens();
-    addLogoClickListener();
 
     $.getJSON('/dist/json/locations.json', function(data) {
         locationsArray = data.locations;
+        initMap(locationsArray);
+
+
     });
 
 });
@@ -47,9 +49,9 @@ function initValidation (form) {
         if (validateNumber(field, errorSpan) === false) {
             return false;
         }
-        if (validateLocation(field, errorSpan) === false) {
-            return false;
-        }
+        // if (validateLocation(field, errorSpan) === false) {
+        //     return false;
+        // }
         return true;
     };
 
@@ -80,32 +82,32 @@ function initValidation (form) {
         }
     }
 
-    function validateLocation (field, errorSpan) {
-        if (form.id === "location-input" && isNumber(field.value)) {
-            field.value = '';
-            alert('Please type text only!');
-            return false;
-        }
-        if ( form.id === "location-input" && !isLocation(field.value)) {
-            addErrorSpan(field, errorSpan);
-            errorSpan.innerHTML = "Please select one of the options";
-            return false;
-        }
-        if ( form.id === "location-input" && form[0].value === form[1].value ) {
-            addErrorSpan(field, errorSpan);
-            errorSpan.innerHTML = "Please select a different option";
-            return false;
-        }
-    }
+    // function validateLocation (field, errorSpan) {
+    //     if (form.id === "location-input" && isNumber(field.value)) {
+    //         field.value = '';
+    //         alert('Please type text only!');
+    //         return false;
+    //     }
+    //     if ( form.id === "location-input" && !isLocation(field.value)) {
+    //         addErrorSpan(field, errorSpan);
+    //         errorSpan.innerHTML = "Please select one of the options";
+    //         return false;
+    //     }
+    //     if ( form.id === "location-input" && form[0].value === form[1].value ) {
+    //         addErrorSpan(field, errorSpan);
+    //         errorSpan.innerHTML = "Please select a different option";
+    //         return false;
+    //     }
+    // }
 
-    function isLocation (input) {
-        const locations = locationsArray;
-        for (let i = 0; i < locations.length; i++) {
-            if (locations[i].title === input) {
-                return true;
-            }
-        }
-    }
+    // function isLocation (input) {
+    //     const locations = locationsArray;
+    //     for (let i = 0; i < locations.length; i++) {
+    //         if (locations[i].title === input) {
+    //             return true;
+    //         }
+    //     }
+    // }
 
     function isNumber (input) {
         return /^[0-9]*$/.test(input);
@@ -120,12 +122,8 @@ function initValidation (form) {
         return ['submit', 'reset', 'button', 'hidden', 'fieldset'].indexOf(field.type) === -1;
     }
 
-    if (isError === false && form.id === "user-input" ) {
+    if (isError === false) {
         displayUserDetail();
-    }
-    if (isError === false && form.id === "location-input" ) {
-        console.log('hi');
-        displayMap();
     }
 }
 
@@ -133,12 +131,12 @@ function initValidation (form) {
 // INIT FILTER --------------------------------------------------------------- //
 
 function initFilter() {
-    $('form').on('keyup change', function(e) {
+    $("#user-input").on('keyup select change', function(e) {
         e.preventDefault();
-        $('.top__text').html('');
         let form = $(this)[0];
+        $('.top__text').html('');
         initValidation(form);
-        addFilterListener(form);
+        addFilterListener(form); 
     });
 }
 
@@ -211,7 +209,6 @@ function addFilterListener() {
             matches.push(vehiclesArray[i]);
         }
     });
-
     if ( matches.length == 0 && (!!day && !!traveler) ) {
         alert('No matches! Try different number.');
     }
@@ -288,8 +285,8 @@ function addSubmitEvent() {
 }
 
 function getUserInput() {
-    userDetail.traveldays = $('#traveldays').val();
-    userDetail.travelers = $('#travelers').val();
+    userDetail.traveldays = ( $('#traveldays').val() ) * 1;
+    userDetail.travelers = ( $('#travelers').val() ) * 1;
 }
 
 
@@ -317,16 +314,120 @@ function getSelectedVehicle(selected) {
 }
 
 function displaySelectedVehicle(vehicle) {
-    getLocationInput();
     const imgSrc = "image/icon" + vehicle.id + ".png";
     $('.vehicle-icon').attr('src', imgSrc);
     $('.vehicle-title').html(vehicle.title);
+    addFinalSubmitEvent();
 }
 
-function getLocationInput() {
-    userDetail.startpoint = $('#startpoint').val();
-    userDetail.endpoint = $('#endpoint').val();
+// DISPLAY SELECT OPTIONS ---------------------------------------------------- //
+
+function displayLocationOptions(locations) {
+    $.each(locations, function(i, location) {
+        $('select').append('<option data-location="' + location.id + '" value="' + location.title + '">' + location.title + '</option>');
+    })
 }
+
+// DISPLAY MAP --------------------------------------------------------------- //
+
+function initMap(locations) {
+    displayLocationOptions(locations);
+
+    const geoPoint = locations[0].coordinates;
+    const mymap = L.map('map').setView(geoPoint, 9);
+
+    L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+        maxZoom: 18,
+        id: 'mapbox/streets-v11',
+        tileSize: 512,
+        zoomOffset: -1,
+        accessToken: 'pk.eyJ1IjoiNXYiLCJhIjoiY2tiaXhjNnFqMGhseTJ5azAycDlmZm05aCJ9.JYtaCI63YUbc0RzpP4GeXA'
+    }).addTo(mymap);
+    addSelectClickEvent();
+}
+
+
+// var marker = L.marker(geo, {
+//     opacity:0.5
+// }).addTo(mymap);
+
+// marker.bindPopup('<h1>Header</h1>');
+
+// distance(L.latLng(point), L.latLng(point))
+// var a = map.distance(L.latLng(-36.85984517196145, 174.79248046875), L.latLng(-42.593532625649935, 172.3095703125));
+// console.log(a);
+
+// function getGeojson(locations) {
+//     $.each(locations, function (i, location) {
+//         getLocationInput();
+//         if (userDetail.startpoint === location.title) {
+//             let b = location.coordinates;
+//             console.log(b);
+//         };
+//     });
+// }
+// function initCreateMaker() {
+//     // $('#startpoint').on('click', function() {
+//     //     console.log('click');
+//     //     $.each(locations, function (i, location) {
+//     //         getLocationInput();
+//     //         if (userDetail.startpoint === location.title) {
+//     //             let b = location.coordinates;
+//     //             console.log(b);
+//     //         };
+//     //     });
+//     // });
+
+//     $('select').on('click', function() {
+//         const point = $(this);
+//         const selectedValue = point.val();
+//         console.log(point);
+//         console.log(point.id);
+//         console.log(selectedValue);
+//     })
+// }
+
+
+// GET LOCATION INPUT -------------------------------------------------------- //
+
+function addSelectClickEvent() {
+    $('select').on('click', function() {
+        const select = $(this);
+        getLocationInput(select);
+        console.log(select.val());
+        initCreateMaker(select.val());
+    })
+}
+
+function getLocationInput(select) {
+    const inputId = select.attr('id');
+    userDetail[inputId] = select.val();
+}
+
+// CREATE MAKER -------------------------------------------------------------- //
+
+// function initCreateMaker() {
+//     $.each()
+// }
+
+
+
+// GET CONFIRMATION ---------------------------------------------------------- //
+
+function addFinalSubmitEvent() {
+    const btn = $('#submit-location');
+    btn.on('click', function(e) {
+        e.preventDefault();
+        let currentScreen = 3;
+        switchScreen(currentScreen);
+        // displayConfirmation();
+    });
+}
+
+// function displayConfirmation() {
+
+// }
 
 
 // SWICH SCREEN -------------------------------------------------------------- //
@@ -335,6 +436,7 @@ function initScreens () {
     const el_screens = $('.screen');
     el_screens.slice(1).hide();
     backToScreen(el_screens);
+    addLogoClickListener();
 }
 
 function switchScreen(screen) {
@@ -358,34 +460,8 @@ function addLogoClickListener () {
     const el_screens = $('.screen');
     $('.header__logo').click(function() {
         el_screens.slice(0,1).show();
-    })
+    });
 }
 
-// DISPLAY MAP --------------------------------------------------------------- //
-
-// var mymap = L.map('map').setView([51.505, -0.09], 5);
-
-// L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-//     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-//     maxZoom: 18,
-//     id: 'mapbox/streets-v11',
-//     tileSize: 512,
-//     zoomOffset: -1,
-//     accessToken: 'pk.eyJ1IjoiNXYiLCJhIjoiY2tiaXhjNnFqMGhseTJ5azAycDlmZm05aCJ9.JYtaCI63YUbc0RzpP4GeXA'
-// }).addTo(mymap);
-
-
-
-// displayMap();
-
-function displayMap() {
-    mapboxgl.accessToken = 'pk.eyJ1IjoiNXYiLCJhIjoiY2tiaXhjNnFqMGhseTJ5azAycDlmZm05aCJ9.JYtaCI63YUbc0RzpP4GeXA';
-    var map = new mapboxgl.Map({
-    container: 'map',
-    style: 'mapbox://styles/mapbox/streets-v11', // stylesheet location
-    center: [174.792, -36.859], // starting position at Auckland
-    zoom: 9 // starting zoom
-    });    
-}
 
 
